e7ffc3f4cffe0da4c63c67f215a557fbcc5256d6
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-09 06:52:10 -0800

    all the things

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 export default defineConfig({
   server: {
+    port: 8000,
     headers: {
       "X-Custom-Header": "value",
       "X-Another-Custom-Header": "anotherValue",
