9cc19ef42bdfa90d75302b0c4b4d0ad65cf68168
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-09 06:41:40 -0800

    magic

--- build/server.ts
+++ build/server.ts
@@ -1,7 +1,9 @@
 // import { serveStatic } from "hono/bun";
-import { serveStatic } from "hono/deno";
-import { Hono } from "hono";
-import { secureHeaders } from "hono/secure-headers";
+import { serveStatic } from "npm:hono/deno";
+import { Hono } from "npm:hono";
+import { secureHeaders } from "npm:hono/secure-headers";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 const app = new Hono();
 console.log("ok...");
 app.use(
@@ -10,6 +12,10 @@ app.use(
     crossOriginEmbedderPolicy: true,
   })
 );
-app.use("/*", serveStatic({ root: "./" }));
+app.use("/*", serveStatic({ root: "./dist" }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/*", serveStatic({ root: join(__dirname, "dist") }));
 
 export default app;
