9464d3142b0256b3067bedf73c0035f68237f2dd
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-09 19:48:35 -0800

    now with svelte

---
+++ App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  function greet() {
+    alert("Welcome to Svelte!");
+  }
+  let { onClick } = $props();
+</script>
+
+<button onclick={onClick}>click me</button>
+
+<style>
+  button {
+    font-size: 2em;
+  }
+</style>
